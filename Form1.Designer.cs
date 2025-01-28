e4b86d358565c340e73c4f2f694290d68b51b5c0
MaximScherbinin <79851740+MaximScherbinin@users.noreply.github.com>
2025-01-28 13:21:14 +0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace hlm2localization_editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            saveAsToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            dataGridView1 = new DataGridView();
+            lang = new DataGridViewTextBoxColumn();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, saveToolStripMenuItem, saveAsToolStripMenuItem, toolStripSeparator1, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(180, 22);
+            openToolStripMenuItem.Text = "Open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(180, 22);
+            saveToolStripMenuItem.Text = "Save";
+            saveToolStripMenuItem.Click += saveToolStripMenuItem_Click;
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            saveAsToolStripMenuItem.Size = new Size(180, 22);
+            saveAsToolStripMenuItem.Text = "Save As...";
+            saveAsToolStripMenuItem.Click += saveAsToolStripMenuItem_Click;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(180, 22);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.BackgroundColor = Color.Plum;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { lang });
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 24);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 30;
+            dataGridView1.Size = new Size(800, 426);
+            dataGridView1.TabIndex = 1;
+            // 
+            // lang
+            // 
+            lang.HeaderText = "English";
+            lang.Name = "lang";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Plum;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "hlm2localization-editor";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem saveAsToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn lang;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,230 @@
+using System.Configuration;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace hlm2localization_editor
+{
+    public partial class Form1 : Form
+    {
+        public struct Language
+        {
+            public uint ID;
+            public uint NameLength;
+            public string Name;
+            public uint Unk0;
+            public uint EntryCount;
+            public uint[] Entries;
+            public string[] Strings;
+        }
+        public struct LocDataTable
+        {
+            public uint LangCount;
+            public Language[] Languages;
+
+        }
+        LocDataTable LocData = new LocDataTable();
+        public string LocFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.Columns.Clear();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+            dataGridView1.Rows.Clear();
+
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "hlm2_localization.bin (*.bin)|*.bin";
+            openFileDialog.FilterIndex = 0;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                filePath = openFileDialog.FileName;
+                LocFile = openFileDialog.FileName;
+
+                //Read the contents of the file into a stream
+                var fileStream = openFileDialog.OpenFile();
+
+                using (var reader = new BinaryReader(fileStream))
+                {
+                    LocData.LangCount = reader.ReadUInt32();
+                    LocData.Languages = new Language[LocData.LangCount];
+
+                    for (int l = 0; l < LocData.LangCount; l++)
+                    {
+                        var Lang = new Language();
+
+                        Lang.ID = reader.ReadUInt32();
+                        Lang.NameLength = reader.ReadUInt32();
+                        Lang.Name = new string(reader.ReadChars((int)Lang.NameLength));
+                        Lang.Unk0 = reader.ReadUInt32();
+                        Lang.EntryCount = reader.ReadUInt32();
+                        Lang.Entries = new uint[Lang.EntryCount + 1];
+                        Lang.Strings = new string[Lang.EntryCount];
+                        for (int i = 0; i < Lang.EntryCount + 1; i++)
+                        {
+                            Lang.Entries[i] = reader.ReadUInt32();
+                        }
+                        for (int i = 0; i < Lang.EntryCount; i++)
+                        {
+                            // strlen()
+                            string s = string.Empty;
+                            char c;
+                            do
+                            {
+                                c = reader.ReadChar();
+                                s += c;
+                            } while (c != '\0');
+                            Lang.Strings[i] = s;
+                        }
+                        LocData.Languages[l] = Lang;
+                    }
+
+                }
+
+                for (int i = 0; i < LocData.LangCount; i++)
+                {
+                    Language lang = LocData.Languages[i];
+                    dataGridView1.Columns.Add(lang.ID.ToString(), lang.Name);
+                    dataGridView1.Columns[0].ReadOnly = true;
+                    dataGridView1.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+                }
+                for (int i = 0; i < LocData.Languages[0].Strings.Length; i++)
+                {
+                    List<string> strs = new List<string>();
+                    for (int j = 0; j < LocData.LangCount; j++)
+                    {
+                        strs.Add(LocData.Languages[j].Strings[i]);
+                    }
+                    dataGridView1.Rows.Add(strs.ToArray());
+                }
+            }
+
+            //MessageBox.Show($"Lang count: {LocData.LangCount}\n{LocData.Languages[0].ID}\n{LocData.Languages[0].NameLength}\n{LocData.Languages[0].Name}\n{LocData.Languages[0].EntryCount}\n{LocData.Languages[0].Entries[0]}\n{LocData.Languages[0].Strings[0]}", "SUCCESS!", MessageBoxButtons.OK);
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+            Application.Exit();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Columns.Count == 0)
+            {
+                MessageBox.Show("No file.", "Error", MessageBoxButtons.OK);
+                return;
+            }
+            SaveFile();
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Columns.Count == 0)
+            {
+                MessageBox.Show("No file.", "Error", MessageBoxButtons.OK);
+                return;
+            }
+
+            Stream stream;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "bin files (*.bin)|*.bin";
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                SaveFile(saveFileDialog.OpenFile());
+            }
+
+        }
+
+        void SaveFile(Stream? stream = null)
+        {
+            //Applying changes
+            for (int i = 0; i < LocData.Languages[0].Strings.Length; i++)
+            {
+                for (int j = 0; j < LocData.LangCount; j++)
+                {
+                    LocData.Languages[j].Strings[i] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+
+                    if (LocData.Languages[j].Strings[i][LocData.Languages[j].Strings[i].Length - 1] != '\0') 
+                    {
+                        LocData.Languages[j].Strings[i] += '\0';
+                    }
+                }
+            }
+
+
+            //Saving
+
+
+            //var fn = LocFile;
+            //fn.Replace(".bin", "_edit.bin");
+            Stream saveStream = null;
+            if (stream != null)
+            {
+                saveStream = stream;
+            } else
+            {
+                saveStream = File.Open(LocFile, FileMode.Create);
+            }
+            
+            {
+                using (var writer = new BinaryWriter(saveStream))
+                {
+                    writer.Write(LocData.LangCount);
+                    for (int i = 0; i < LocData.LangCount; i++)
+                    {
+                        Language Lang = LocData.Languages[i];
+                        writer.Write(Lang.ID);
+                        writer.Write(Lang.NameLength);
+                        string s = string.Empty;
+                        byte[] _utf16Bytes = Encoding.Unicode.GetBytes(Lang.Name);
+                        byte[] _utf8Bytes = Encoding.Convert(Encoding.Unicode, Encoding.UTF8, _utf16Bytes);
+                        for (int b = 0; b < _utf8Bytes.Length; b++)
+                        {
+                            writer.Write(_utf8Bytes[b]);
+                        }
+                        writer.Write(Lang.Unk0);
+                        writer.Write(Lang.EntryCount);
+                        uint dataPos = 0;
+                        writer.Write(dataPos);
+                        //uint[] Entries;
+                        for (int j = 0; j < Lang.Strings.Length; j++)
+                        {
+                            byte[] utf16Bytes = Encoding.Unicode.GetBytes(Lang.Strings[j]);
+                            byte[] utf8Bytes = Encoding.Convert(Encoding.Unicode, Encoding.UTF8, utf16Bytes);
+                            dataPos += Convert.ToUInt32(utf8Bytes.Length);
+                            writer.Write(dataPos);
+                        }
+
+                        for (int j = 0; j < Lang.Strings.Length; j++)
+                        {
+                            byte[] utf16Bytes = Encoding.Unicode.GetBytes(Lang.Strings[j]);
+                            byte[] utf8Bytes = Encoding.Convert(Encoding.Unicode, Encoding.UTF8, utf16Bytes);
+                            for (int b = 0; b < utf8Bytes.Length; b++) 
+                            {
+                                writer.Write(utf8Bytes[b]);
+                            }
+                            //writer.Write(new byte());
+                        }
+                    }
+                }
+            }
+            saveStream.Close();
+            //stream.Close();
+            MessageBox.Show("sUCCESS!", "File Saved!", MessageBoxButtons.OK);
+        }
+    }
+}
